623b916efc9e52220d333d3e5c7de3cbde5457db
Kavimal Wijewardana <kavi707@gmail.com>
2016-08-09 18:39:12 +0530

    Number picker slider

--- app/src/main/java/com/kavi/droid/emenu/activities/TableSelectActivity.java
+++ app/src/main/java/com/kavi/droid/emenu/activities/TableSelectActivity.java
@@ -1,12 +1,18 @@
 package com.kavi.droid.emenu.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import com.kavi.droid.emenu.R;
+import com.kavi.droid.emenu.adapters.NumberPickerItemAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by kavi707 on 8/4/16.
@@ -15,6 +21,9 @@ import com.kavi.droid.emenu.R;
 public class TableSelectActivity extends Activity {
 
     private Button continueBtn;
+    private RecyclerView horizontalListView;
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,7 @@ public class TableSelectActivity extends Activity {
     private void setUpViews() {
 
         continueBtn = (Button) findViewById(R.id.continueBtn);
+        horizontalListView = (RecyclerView) findViewById(R.id.numberPicker);
 
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +45,41 @@ public class TableSelectActivity extends Activity {
                 startActivity(menuIntent);
             }
         });
+
+        ArrayList<String> horizontalList= new ArrayList<>();
+
+        horizontalList.add("");
+        horizontalList.add("");
+        horizontalList.add("");
+        horizontalList.add("01");
+        horizontalList.add("02");
+        horizontalList.add("03");
+        horizontalList.add("04");
+        horizontalList.add("05");
+        horizontalList.add("06");
+        horizontalList.add("07");
+        horizontalList.add("08");
+        horizontalList.add("09");
+        horizontalList.add("10");
+        horizontalList.add("11");
+        horizontalList.add("12");
+        horizontalList.add("13");
+        horizontalList.add("14");
+        horizontalList.add("15");
+        horizontalList.add("16");
+        horizontalList.add("17");
+        horizontalList.add("18");
+        horizontalList.add("19");
+        horizontalList.add("20");
+        horizontalList.add("");
+        horizontalList.add("");
+        horizontalList.add("");
+
+        NumberPickerItemAdapter numberPickerItemAdapter = new NumberPickerItemAdapter(horizontalList, context);
+
+        LinearLayoutManager horizontalLayoutManagaer
+                = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        horizontalListView.setLayoutManager(horizontalLayoutManagaer);
+        horizontalListView.setAdapter(numberPickerItemAdapter);
     }
 }
---
+++ app/src/main/java/com/kavi/droid/emenu/adapters/NumberPickerItemAdapter.java
@@ -0,0 +1,81 @@
+package com.kavi.droid.emenu.adapters;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kavi.droid.emenu.R;
+
+import java.util.List;
+
+/**
+ * Created by kwijewardana on 8/9/16.
+ */
+public class NumberPickerItemAdapter extends RecyclerView.Adapter<NumberPickerItemAdapter.PickerItemViewHolder> {
+
+    private List<String> horizontalList;
+    private Context context;
+    private int midPos = 3;
+
+    public NumberPickerItemAdapter(List<String> horizontalList, Context context) {
+        this.horizontalList = horizontalList;
+        this.context = context;
+    }
+
+    @Override
+    public PickerItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_picker_item, parent, false);
+
+        return new PickerItemViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(PickerItemViewHolder holder, int position) {
+        final PickerItemViewHolder viewHolder = holder;
+        holder.numberTextView.setText(horizontalList.get(position));
+
+        if (position == midPos && !horizontalList.get(position).equals("")) {
+            holder.numberTextView.setTextColor(context.getResources().getColor(R.color.lightPurple));
+            holder.numberTextView.setTextSize(100);
+            holder.numberTextView.setTypeface(Typeface.DEFAULT);
+            holder.pickerItemLinearLayout.setBackground(context.getResources().getDrawable(R.drawable.table_select_box));
+        } else {
+            holder.numberTextView.setTextColor(context.getResources().getColor(R.color.colorDarkGrey));
+            holder.numberTextView.setTextSize(80);
+            holder.numberTextView.setTypeface(Typeface.DEFAULT_BOLD);
+            holder.pickerItemLinearLayout.setBackground(null);
+        }
+
+        holder.numberTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Selected item + " + viewHolder.numberTextView.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return horizontalList.size();
+    }
+
+    public class PickerItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView numberTextView;
+        public LinearLayout pickerItemLinearLayout;
+
+        public PickerItemViewHolder(View view) {
+            super(view);
+            numberTextView = (TextView) view.findViewById(R.id.numberTextView);
+            pickerItemLinearLayout = (LinearLayout) view.findViewById(R.id.pickerItemLinearLayout);
+        }
+    }
+}
