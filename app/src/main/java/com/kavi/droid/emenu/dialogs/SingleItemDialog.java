7fc2783f51fdae91a15fd9c84f35369904c3f06b
Kavimal Wijewardana <kavi707@gmail.com>
2016-08-22 14:35:11 +0530

    Add close button to single item view dialog

--- app/src/main/java/com/kavi/droid/emenu/dialogs/SingleItemDialog.java
+++ app/src/main/java/com/kavi/droid/emenu/dialogs/SingleItemDialog.java
@@ -3,7 +3,10 @@ package com.kavi.droid.emenu.dialogs;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.kavi.droid.emenu.R;
 
@@ -13,6 +16,7 @@ import com.kavi.droid.emenu.R;
  */
 public class SingleItemDialog extends Dialog {
 
+    private ImageButton closeImageButton;
     private Context context;
 
     public SingleItemDialog(Context context) {
@@ -25,5 +29,18 @@ public class SingleItemDialog extends Dialog {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_single_item);
+
+        setUpView();
+    }
+
+    private void setUpView() {
+
+        closeImageButton = (ImageButton) findViewById(R.id.closeImageButton);
+        closeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
     }
 }
